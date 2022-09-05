8741e4d31a01a7808c470f3f1766088fabd718df
Ceyda Elmas <79998344+ceydaelmas@users.noreply.github.com>
2022-09-05 17:01:03 +0300

    Login and Register added to MVC, and startup template entegrated

---
+++ GriffonCMS/Presentation/GriffonCMS.WebUI/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using GriffonCMS.Application.Interfaces;
+using GriffonCMS.Infrastructure.DTOS.Account;
+using GriffonCMS.WebUI.Controllers.Base;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GriffonCMS.WebUI.Areas.Admin.Controllers;
+[Area("Admin")]
+public class LoginController : BaseController
+{
+    private IAuthService _authService;
+    public LoginController(ILogger<BaseController> logger, ISender sender, IAuthService authService) : base(logger, sender)
+    {
+        _authService = authService;
+    }
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Login(LoginRequest loginRequest)
+    {
+        var adminToLogin = _authService.Login(loginRequest);
+        if (!adminToLogin.Success)
+        {
+            return View();
+        }
+        var result = _authService.CreateAccessToken(adminToLogin.Data);
+        if (result.Success)
+        {
+            return RedirectToAction("Index", "DashBoard",new { Area = "Admin" });
+        }
+        return View();
+    }
+}
