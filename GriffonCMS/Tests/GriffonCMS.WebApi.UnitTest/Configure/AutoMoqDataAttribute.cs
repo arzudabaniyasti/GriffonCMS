d5ad43b2d1e6d25f88b39ffa5a7b45985c91ec82
arzudabaniyasti <dabaniyastiarzu@gmail.com>
2023-03-23 16:58:03 +0300

    ✅ Add success test scenario for post about endpoint

---
+++ GriffonCMS/Tests/GriffonCMS.WebApi.UnitTest/Configure/AutoMoqDataAttribute.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace Ecommerce.Unit.Configure;
+
+public class AutoMoqDataAttribute : AutoDataAttribute
+{
+    public AutoMoqDataAttribute()
+    : base(() => new Fixture().Customize(new AutoMoqCustomization()))
+    {
+
+    }
+}
---
+++ GriffonCMS/Tests/GriffonCMS.WebApi.UnitTest/Controllers/AboutControllerTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using Ecommerce.Unit.Configure;
+using MediatR;
+using WebApi.Controllers;
+using GriffonCMS.Infrastructure.Command.Abouts;
+using FluentAssertions;
+
+namespace GriffonCMS.WebApi.UnitTest.Controllers;
+public class AboutControllerTest
+{
+    [Theory, AutoMoqData]
+    public async Task Post_About_Return_Ok_Object_Result(Mock<IMediator> mediator,CreateAboutCommand actualQuery,
+        CreateAboutCommand actualCommand,Guid expected)
+    {
+        //Arrange
+
+        var sut = new AboutController(mediator.Object);
+
+        mediator.Setup(setup => setup.Send(actualQuery, default)).ReturnsAsync(expected);
+
+        //Act
+
+        var result = await sut.Post(actualCommand);
+
+        //Assert
+
+        Assert.IsType<OkObjectResult>(result);
+    }
+
+    [Theory, AutoMoqData]
+    public async Task Post_About_Return_Guid(Mock<IMediator> mediator, CreateAboutCommand actualQuery,
+    CreateAboutCommand actualCommand, Guid expected)
+    {
+        //Arrange
+
+        var sut = new AboutController(mediator.Object);
+
+        mediator.Setup(setup => setup.Send(actualQuery, default)).ReturnsAsync(expected);
+
+        //Act
+
+        var result = await sut.Post(actualCommand);
+
+        //Assert
+
+        var apiOkResult = result.Should().BeOfType<OkObjectResult>().Subject;
+        var response =apiOkResult.Value;
+        Assert.IsType<Guid>(response);
+    }
+}
